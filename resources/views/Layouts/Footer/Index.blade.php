bfc792938309cdd64aa12d91f0b88e47729cccfb
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-01 22:41:51 +0800

    feat: added footer component

---
+++ resources/views/Layouts/Footer/Index.blade.php
@@ -0,0 +1,28 @@
+<footer class="footer footer-transparent d-print-none">
+	<div class="container-xl">
+		<div class="row text-center align-items-center flex-row-reverse">
+			<div class="col-lg-auto ms-lg-auto">
+				<ul class="list-inline list-inline-dots mb-0">
+					<li class="list-inline-item">
+						<a href="#" target="_blank" class="link-secondary">SISTEM INFORMASI MANAJEMEN ASET</a>
+					</li>
+					<li class="list-inline-item">
+						<a href="https://www.instagram.com/pmkstikombali/" target="_blank" class="link-secondary"
+							rel="noopener">
+							PT TELKOM INFRA AREA BALI TIMUR
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-12 col-lg-auto mt-3 mt-lg-0">
+				<ul class="list-inline list-inline-dots mb-0">
+					<li class="list-inline-item">
+						Copyright &copy;2023
+						<a href="#" class="link-secondary">MOH HAJINUL HAKIM</a>.
+						All rights reserved.
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
