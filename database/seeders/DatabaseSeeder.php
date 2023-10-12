42a1b406e602ff4788095d9c82a6faefbde822a6
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-12 22:48:41 +0800

    feat: added DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +15,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'username' => 'hajinulhakim',
+            'email' => 'hakim@gmail.com',
+            'password' => 'password', //password
+            'whatsapp' => '085858038176',
+            'foto' => null,
+            'remember_token' => Str::random(10),
+            'email_verified_at' => now(),
+        ]);
     }
 }
