958c4ed6c179969a9aa221e0ebb32ad0f18d2a28
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-12 22:48:24 +0800

    feat: migration 2014_10_12_000000_create_users_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('whatsapp')->unique();
+            $table->string('foto')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
