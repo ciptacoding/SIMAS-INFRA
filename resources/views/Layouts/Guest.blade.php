4f31b0df824fc16dc91a7688fd4802801a462c71
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-12 22:49:44 +0800

    Feat: added form validation

--- resources/views/Layouts/Guest.blade.php
+++ resources/views/Layouts/Guest.blade.php
@@ -4,7 +4,7 @@
 <head>
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1">
-   <title>AdminLTE 3 | Log in</title>
+   <title>SIMAS | Log in</title>
 
    <!-- Google Font: Source Sans Pro -->
    <link rel="stylesheet"
@@ -20,24 +20,30 @@
 <body class="hold-transition login-page">
    <div class="login-box">
       <div class="login-logo">
-         <a href="../../index2.html"><b>Admin</b>LTE</a>
+         <a href="#"><img src="{{ asset('Logo/Logo-TELKOM.png') }}" width="200" alt="logo"></a>
+         <p class="text-sm">Sistem Informasi Manajemen Aset PT. Telkom Infra</p>
       </div>
       <!-- /.login-logo -->
       <div class="card">
          <div class="card-body login-card-body">
             <p class="login-box-msg">Sign in to start your session</p>
 
-            <form action="../../index3.html" method="post">
+            <form action="{{ route('authenticate') }}" method="POST">
+               @csrf
                <div class="input-group mb-3">
-                  <input type="email" class="form-control" placeholder="Email">
+                  <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email"
+                     name="email">
                   <div class="input-group-append">
                      <div class="input-group-text">
                         <span class="fas fa-envelope"></span>
                      </div>
                   </div>
+                  @error('email')
+                  <span class="invalid-feedback">{{ $message }}</span>
+                  @enderror
                </div>
                <div class="input-group mb-3">
-                  <input type="password" class="form-control" placeholder="Password">
+                  <input type="password" class="form-control" placeholder="Password" name="password">
                   <div class="input-group-append">
                      <div class="input-group-text">
                         <span class="fas fa-lock"></span>
@@ -47,7 +53,7 @@
                <div class="row">
                   <div class="col-8">
                      <div class="icheck-primary">
-                        <input type="checkbox" id="remember">
+                        <input type="checkbox" id="remember" name="remember">
                         <label for="remember">
                            Remember Me
                         </label>
@@ -55,28 +61,15 @@
                   </div>
                   <!-- /.col -->
                   <div class="col-4">
-                     <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                     <button type="submit" class="btn btn-dark btn-block">Sign In</button>
                   </div>
                   <!-- /.col -->
                </div>
             </form>
-
-            <div class="social-auth-links text-center mb-3">
-               <p>- OR -</p>
-               <a href="#" class="btn btn-block btn-primary">
-                  <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-               </a>
-               <a href="#" class="btn btn-block btn-danger">
-                  <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-               </a>
-            </div>
             <!-- /.social-auth-links -->
 
             <p class="mb-1">
-               <a href="forgot-password.html">I forgot my password</a>
-            </p>
-            <p class="mb-0">
-               <a href="register.html" class="text-center">Register a new membership</a>
+               <a href="#">I forgot my password</a>
             </p>
          </div>
          <!-- /.login-card-body -->
