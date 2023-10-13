ec29e8a8376818d547be09ac209c7c3047250444
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-13 14:51:32 +0800

    Feat: create SuperAdminMiddleware

--- app/Http/Middleware/SuperAdminMiddleware.php
+++ app/Http/Middleware/SuperAdminMiddleware.php
@@ -15,6 +15,10 @@ class SuperAdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if ($request->user() && $request->user()->role_id === 1) {
+            return $next($request);
+        }
+
+        return redirect('/');
     }
 }
