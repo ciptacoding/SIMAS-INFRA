33c8096e4cde4c31ff16db7e5804a08e127d3e6f
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-16 18:58:24 +0800

    Feat: added menu super admin in sidebar

--- resources/views/Layouts/Sidebar/Index.blade.php
+++ resources/views/Layouts/Sidebar/Index.blade.php
@@ -13,6 +13,8 @@
          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+
+            <li class="nav-header">Internal</li>
             <li class="nav-item">
                <a href="#" class="nav-link active">
                   <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -26,19 +28,94 @@
                <a href="pages/widgets.html" class="nav-link">
                   <i class="nav-icon fas fa-th"></i>
                   <p>
-                     Widgets
+                     Pengguna
                   </p>
                </a>
             </li>
-            <li class="nav-header">EXAMPLES</li>
+            <li class="nav-item">
+               <a href="pages/widgets.html" class="nav-link">
+                  <i class="nav-icon fas fa-th"></i>
+                  <p>
+                     Data Tower
+                  </p>
+               </a>
+            </li>
+            <li class="nav-item">
+               <a href="pages/widgets.html" class="nav-link">
+                  <i class="nav-icon fas fa-th"></i>
+                  <p>
+                     Aset Tower
+                  </p>
+               </a>
+            </li>
+            <li class="nav-header">Eksternal</li>
             <li class="nav-item">
                <a href="pages/calendar.html" class="nav-link">
                   <i class="nav-icon far fa-calendar-alt"></i>
                   <p>
-                     Calendar
+                     Maintenance
                      <span class="badge badge-info right">2</span>
                   </p>
                </a>
+            </li>
+            <li class="nav-item">
+               <a href="pages/calendar.html" class="nav-link">
+                  <i class="nav-icon far fa-calendar-alt"></i>
+                  <p>
+                     Kelola Notifikasi
+                     <span class="badge badge-info right">2</span>
+                  </p>
+               </a>
+            </li>
+            <li class="nav-item">
+               <a href="pages/calendar.html" class="nav-link">
+                  <i class="nav-icon far fa-calendar-alt"></i>
+                  <p>
+                     Data Team
+                     <span class="badge badge-info right">2</span>
+                  </p>
+               </a>
+            </li>
+            <li class="nav-item">
+               <a href="#" class="nav-link">
+                  <i class="nav-icon fas fa-tree"></i>
+                  <p>
+                     Data Laporan
+                     <i class="fas fa-angle-left right"></i>
+                  </p>
+               </a>
+               <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                     <a href="pages/UI/general.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Laporan Maintenance</p>
+                     </a>
+                  </li>
+                  <li class="nav-item">
+                     <a href="pages/UI/icons.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Laporan Sparepart</p>
+                     </a>
+                  </li>
+                  <li class="nav-item">
+                     <a href="pages/UI/buttons.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Request Maintenance</p>
+                     </a>
+                  </li>
+                  <li class="nav-item">
+                     <a href="pages/UI/sliders.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Laporan Lokasi Tower</p>
+                     </a>
+                  </li>
+                  <li class="nav-item">
+                     <a href="pages/UI/modals.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Laporan Data Team</p>
+                     </a>
+                  </li>
+               </ul>
             </li>
          </ul>
       </nav>
