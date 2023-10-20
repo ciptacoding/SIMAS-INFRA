6bb5b4ee437bd4e747b3a8adb6a85413e0e424df
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-20 11:26:24 +0800

    feat: create relationship in Team model

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Team extends Model
 {
     use HasFactory;
+
+    // Reverse relationship between table teams and users
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relationship 1 to many between table teams and maintenances
+    public function maintenances(): HasMany
+    {
+        return $this->hasMany(Maintenance::class);
+    }
+
+    // Relationship 1 to many between table towers and penugasans
+    public function penugasans(): HasMany
+    {
+        return $this->hasMany(Penugasan::class);
+    }
 }
