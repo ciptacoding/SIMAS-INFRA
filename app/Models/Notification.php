0aa5fdd90dde9f6f11e3cf913f0d61a18074aaf8
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-20 11:25:22 +0800

    feat: create relationship in Notification model

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Notification extends Model
 {
     use HasFactory;
+
+    // Reverse relationship between table notifications and users
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
