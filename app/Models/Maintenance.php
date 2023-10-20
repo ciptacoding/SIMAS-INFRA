02067cab4ac9fafafa7fb96aab46daa6af64de8f
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-20 11:25:11 +0800

    feat: create relationship in Maintenance model

--- app/Models/Maintenance.php
+++ app/Models/Maintenance.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Maintenance extends Model
 {
     use HasFactory;
+
+    // Reverse relationship between table maintenances and teams
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    // Reverse relationship between table maintenances and towers
+    public function tower(): BelongsTo
+    {
+        return $this->belongsTo(Tower::class);
+    }
 }
