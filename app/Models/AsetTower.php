fa551a16215e51f93083ef5f9574d2ac39ea953f
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-20 11:24:42 +0800

    feat: create relationship in AsetTower model

--- app/Models/AsetTower.php
+++ app/Models/AsetTower.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AsetTower extends Model
 {
     use HasFactory;
+
+    // Reverse relationship between table aset_towers and tower
+    public function tower(): BelongsTo
+    {
+        return $this->belongsTo(Tower::class);
+    }
 }
