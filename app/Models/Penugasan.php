ea55b1c27bd5620d83fad383307d12d0765b0dd6
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-20 11:26:03 +0800

    feat: create relationship in Penugasan model

--- app/Models/Penugasan.php
+++ app/Models/Penugasan.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Penugasan extends Model
 {
     use HasFactory;
+
+    // Reverse relationship between table penugasans and teams
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
 }
