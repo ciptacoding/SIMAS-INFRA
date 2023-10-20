1978705cb5d92c76c7c895383c6d36c29aec1072
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-20 11:26:34 +0800

    feat: create relationship in Tower model

--- app/Models/Tower.php
+++ app/Models/Tower.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tower extends Model
 {
     use HasFactory;
+
+    // Relationship 1 to many between table towers and maintenances
+    public function maintenances(): HasMany
+    {
+        return $this->hasMany(Maintenance::class);
+    }
+
+    // Relationship 1 to many between table towers and aset_towers
+    public function asetTowers(): HasMany
+    {
+        return $this->hasMany(AsetTower::class);
+    }
 }
