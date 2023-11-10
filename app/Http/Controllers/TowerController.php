ab45340b5b9533b6d57669be0da3da58e083946d
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-11-10 15:07:29 +0800

    Feat: added method simpan in TowerController

--- app/Http/Controllers/TowerController.php
+++ app/Http/Controllers/TowerController.php
@@ -2,21 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tower;
 use Illuminate\Http\Request;
 
 class TowerController extends Controller
 {
     public function index()
     {
-        return view('Pages.Tower.Index');
+        $towers = Tower::all();
+        return view('Pages.Tower.Index', compact('towers'));
     }
 
-    public function create($latitude, $longtitude)
+    public function markers()
+    {
+        $markers = Tower::all();
+        return response()->json($markers);
+    }
+
+    public function tambah($latitude, $longtitude)
     {
         $dataLocation = [
             "latitude" => $latitude,
             "longtitude" => $longtitude,
         ];
-        return view('Pages.Tower.Create', compact('dataLocation'));
+        return view('Pages.Tower.Tambah', compact('dataLocation'));
+    }
+
+    public function simpan(Request $request)
+    {
+        $request->validate([
+            'nama_tower' => ['required', 'max:255'],
+            'lokasi_tower' => ['required', 'max:255'],
+            'latitude' => ['required'],
+            'longtitude' => ['required'],
+            'status_tower' => ['required', 'max:255']
+        ]);
+
+        Tower::create([
+            'nama_tower' => $request->nama_tower,
+            'lokasi_tower' => $request->lokasi_tower,
+            'latitude' => $request->latitude,
+            'longtitude' => $request->longtitude,
+            'status_tower' => $request->status_tower,
+        ]);
+
+        return redirect()->route('tower.index');
     }
 }
