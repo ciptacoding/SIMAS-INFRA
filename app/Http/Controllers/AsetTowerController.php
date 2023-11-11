a4669ce80e0ab34f0857b63e8e058b7818c74dc4
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-11-11 21:20:09 +0800

    Feat: create AsetTowerController

--- app/Http/Controllers/AsetTowerController.php
+++ app/Http/Controllers/AsetTowerController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\AsetTower;
 
 class AsetTowerController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('Pages.AsetTower.Index');
+    }
+
+    public function detail()
+    {
+    }
+
+    public function tambah()
+    {
+    }
+
+    public function simpan()
+    {
+    }
+
+    public function edit()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
 }
