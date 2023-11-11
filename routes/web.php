abd3cb4b51cb7f8cb061438f5a3497d73d815e18
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-11-11 21:19:41 +0800

    Feat: added new routes to AsetTower

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AsetTowerController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
@@ -38,4 +39,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/edit-tower/{id}', [TowerController::class, 'edit'])->name('tower.edit');
     Route::post('/simpan-tower', [TowerController::class, 'simpan'])->name('tower.simpan');
     Route::put('/update-tower/{id}', [TowerController::class, 'update'])->name('tower.update');
+
+    Route::get('/aset-tower', [AsetTowerController::class, 'index'])->name('aset.index');
+    Route::get('/aset-tower/{id}', [AsetTowerController::class, 'detail'])->name('aset.detail');
+    Route::get('/tambah-aset', [AsetTowerController::class, 'tambah'])->name('aset.tambah');
+    Route::get('/edit-aset/{id}', [AsetTowerController::class, 'edit'])->name('aset.edit');
+    Route::post('/simpan-aset', [AsetTowerController::class, 'simpan'])->name('aset.simpan');
+    Route::put('/update-aset/{id}', [AsetTowerController::class, 'update'])->name('aset.update');
+    Route::delete('/delete-aset/{id}', [AsetTowerController::class, 'update'])->name('aset.delete');
 });
