d19a2a7ff711f4e3374f15355c824502d77f31ab
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-11-15 23:20:05 +0800

    Feat: added comment in Controllers

--- app/Http/Controllers/MaintenanceController.php
+++ app/Http/Controllers/MaintenanceController.php
@@ -7,32 +7,39 @@ use App\Models\Maintenance;
 
 class MaintenanceController extends Controller
 {
+    // method untuk menampilkan halaman utama maintenance
     public function index()
     {
         $maintenances = Maintenance::all();
         return view('Pages.Maintenance.Index', compact('maintenances'));
     }
 
+    // method untuk menampilkan halaman detail maintenance
     public function detail($id)
     {
     }
 
+    // method untuk menampilkan form tambah maintenance
     public function tambah()
     {
     }
 
+    // method untuk menyimpan hasil dari tambah maintenance
     public function simpan(Request $request)
     {
     }
 
+    // method untuk menampilkan form edit maintenance
     public function edit(Request $request, $id)
     {
     }
 
+    // method untuk menyimpan hasil edit maintenance
     public function update(Request $request, $id)
     {
     }
 
+    // method untuk melakukan delete item maintenance
     public function delete($id)
     {
     }
