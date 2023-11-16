f384fb482ce5475401f80905de5b39272f8101a8
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-11-16 17:51:08 +0800

    Feat: added several method in TeamController

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Team;
 
 class TeamController extends Controller
 {
-    //
+    public function index()
+    {
+        $teams = Team::all();
+        return view('Pages.Team.Index', compact('teams'));
+    }
+
+    public function detail($id)
+    {
+    }
+
+    public function tambah()
+    {
+    }
+
+    public function simpan(Request $request)
+    {
+    }
+
+    public function edit(Request $request, $id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function delete($id)
+    {
+    }
 }
