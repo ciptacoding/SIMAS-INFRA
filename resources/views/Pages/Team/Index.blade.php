1d302680447f2c14472507fe57cafbdadb04b54f
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-11-17 16:31:56 +0800

    Feat: create looping in datatable

--- resources/views/Pages/Team/Index.blade.php
+++ resources/views/Pages/Team/Index.blade.php
@@ -31,26 +31,20 @@
             </tr>
          </thead>
          <tbody>
-            {{-- @foreach ($assetsTower as $asetTower )
+            @foreach ($teams as $team )
             <tr>
-               <td>{{ $asetTower->tower->nama_tower }}</td>
-               <td>{{ $asetTower->tower->lokasi_tower }}</td>
-               <td>{{ $asetTower->nama_sparepart }}</td>
-               <td>{{ $asetTower->keterangan }}</td>
+               <td>{{ $team->nama_team }}</td>
+               <td>{{ $team->user->username }}</td>
+               <td>{{ $team->tugas }}</td>
+               <td>{{ $team->jumlah_anggota }}</td>
                <td>
-                  <span
-                     class="{{ $asetTower->tower->status_tower === 'aktif' ? 'badge badge-success' : 'badge badge-danger' }}">
-                     {{ $asetTower->tower->status_tower}}
-                  </span>
-               </td>
-               <td>
-                  <a href="{{ route('aset.edit', $asetTower->id) }}" class="btn btn-sm btn-warning">
+                  <a href="{{ route('teams.edit', $team->id) }}" class="btn btn-sm btn-warning">
                      <i class="fas fa-tools" style="color: #ffffff"></i>
                   </a>
-                  <a href="{{ route('aset.detail', $asetTower->id) }}" class="btn btn-sm btn-info">
+                  <a href="{{ route('teams.detail', $team->id) }}" class="btn btn-sm btn-info">
                      <i class="fas fa-info-circle"></i>
                   </a>
-                  <form action="{{ route('aset.delete', $asetTower->id) }}" method="POST" class="d-inline">
+                  <form action="{{ route('teams.delete', $team->id) }}" method="POST" class="d-inline">
                      @method('DELETE')
                      @csrf
                      <button type="submit" class="btn btn-sm btn-danger btn-submit"><i
@@ -58,7 +52,7 @@
                   </form>
                </td>
             </tr>
-            @endforeach --}}
+            @endforeach
          </tbody>
       </table>
    </div>
@@ -68,35 +62,35 @@
 
 @push('scripts')
 <script>
-   // $(function () {
-   //    $('#datatables').DataTable({
-   //       "paging": true,
-   //       "lengthChange": true,
-   //       "searching": true,
-   //       "ordering": true,
-   //       "info": true,
-   //       "autoWidth": true,
-   //       "responsive": true,
-   //    });
-   // });
+   $(function () {
+      $('#datatables').DataTable({
+         "paging": true,
+         "lengthChange": true,
+         "searching": true,
+         "ordering": true,
+         "info": true,
+         "autoWidth": true,
+         "responsive": true,
+      });
+   });
 
-   // $('.btn-submit').click(function (e) {
-   //    e.preventDefault();
-   //    const deleteForm = $(this).closest('form');
+   $('.btn-submit').click(function (e) {
+      e.preventDefault();
+      const deleteForm = $(this).closest('form');
 
-   //    Swal.fire({
-   //       title: "Are you sure?",
-   //       text: "You won't be able to revert this!",
-   //       icon: "warning",
-   //       showCancelButton: true,
-   //       confirmButtonColor: "#3085d6",
-   //       cancelButtonColor: "#d33",
-   //       confirmButtonText: "Yes, delete it!"
-   //    }).then((result) => {
-   //       if (result.isConfirmed) {
-   //          deleteForm.submit();
-   //       }
-   //    });
-   // });
+      Swal.fire({
+         title: "Are you sure?",
+         text: "You won't be able to revert this!",
+         icon: "warning",
+         showCancelButton: true,
+         confirmButtonColor: "#3085d6",
+         cancelButtonColor: "#d33",
+         confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+         if (result.isConfirmed) {
+            deleteForm.submit();
+         }
+      });
+   });
 </script>
 @endpush
