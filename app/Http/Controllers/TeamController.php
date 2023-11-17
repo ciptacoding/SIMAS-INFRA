4bd8636b3e02854c0aff223973029d6ef53d51ba
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-11-17 19:13:34 +0800

    Feat: added method delete in TeamController

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -30,7 +30,7 @@ class TeamController extends Controller
     public function simpan(Request $request)
     {
         $request->validate([
-            'ketua_tim' => ['required'],
+            'ketua_tim' => ['required', 'unique:teams,ketua_tim'],
             'nama_team' => ['required', 'max:255'],
             'tugas' => ['required', 'max:255'],
             'jumlah_anggota' => ['required', 'max:255'],
@@ -46,15 +46,41 @@ class TeamController extends Controller
         return redirect()->route('teams.index');
     }
 
-    public function edit(Request $request, $id)
+    public function edit($id)
     {
+        $team = Team::findOrFail($id);
+        $ketuaTim =
+            User::where('role_id', 3)->whereNotIn('id', function ($query) {
+                $query->select('ketua_tim')->from((new Team)->getTable());
+            })->get();
+        return view('Pages.Team.Edit', compact('team', 'ketuaTim'));
     }
 
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'ketua_tim' => ['required'],
+            'nama_team' => ['required', 'max:255'],
+            'tugas' => ['required', 'max:255'],
+            'jumlah_anggota' => ['required', 'max:255'],
+        ]);
+
+        $team = Team::findOrFail($id);
+
+        $team->update([
+            'ketua_tim' => $request->ketua_tim,
+            'nama_team' => $request->nama_team,
+            'tugas' => $request->tugas,
+            'jumlah_anggota' => $request->jumlah_anggota,
+        ]);
+
+        return redirect()->route('teams.index');
     }
 
     public function delete($id)
     {
+        $team = Team::findOrFail($id);
+        $team->delete();
+        return redirect()->route('teams.index');
     }
 }
