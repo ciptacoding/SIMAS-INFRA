d0cb423e7771587ce576279827214eecbe332226
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-11-25 15:25:14 +0800

    Feat: added page laporan Sparepart

--- resources/views/Pages/Laporan/Sparepart.blade.php
+++ resources/views/Pages/Laporan/Sparepart.blade.php
@@ -41,9 +41,10 @@
                   <a href="{{ route('laporan.edit', $sparepart->id) }}" class="btn btn-sm btn-warning">
                      <i class="fas fa-tools" style="color: #ffffff"></i>
                   </a>
-                  {{-- <a href="{{ route('teams.detail', $team->id) }}" class="btn btn-sm btn-info">
-                     <i class="fas fa-info-circle"></i>
-                  </a> --}}
+                  <a href="{{ route('laporan.download', $sparepart->id) }}" class="btn btn-sm btn-info"
+                     title="Download PDF">
+                     <i class="fas fa-download"></i>
+                  </a>
                   <form action="{{ route('laporan.delete', $sparepart->id) }}" method="POST" class="d-inline">
                      @method('DELETE')
                      @csrf
