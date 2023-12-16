e514f37c367fed9a53cff0a1ad16e1a09217c138
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-12-16 18:51:40 +0800

    Update: add some line of code in Controllers

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Tower;
+use App\Models\AsetTower;
+use App\Models\Team;
+use App\Models\Maintenance;
+use App\Models\Notification;
+use App\Models\Laporan;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('Pages.Dashboard.Index');
+        $pengguna = User::count();
+        $dataTower = Tower::count();
+        $asetTower = AsetTower::count();
+        $team = Team::count();
+        $maintenance = Maintenance::count();
+        $laporan = Laporan::count();
+        return view('Pages.Dashboard.Index', compact('pengguna', 'dataTower', 'asetTower', 'team', 'maintenance', 'laporan'));
     }
 }
--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -41,13 +41,18 @@ class NotificationController extends Controller
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_POSTFIELDS => array(
-                'target' => "$operator->whatsapp, $ketua_tim",
+                'target' => $operator->whatsapp . ',' . $ketua_tim,
                 'message' => 'Hallo, ada notifikasi hasil dari request maintenance telah disetujui oleh super admin dan surat tugas sudah digenerate. Silakan download surat tugas pada menu maintenance.', //sesuaikan pesan
             ),
             CURLOPT_HTTPHEADER => array(
                 'Authorization: cdQtA3F3MnNJmFVqgXk!' //change TOKEN to your actual token
             ),
         ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
         return redirect()->route('notification.index');
     }
 
